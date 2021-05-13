6ba3b1472df96558c256a363d0b02a3fdb92726c
PetyXbronCZ <coolpetr10@gmail.com>
2021-05-13 09:37:27 +0200

    🖍 [0.5.5] - Warnings and errors are written to the console in color
    - Installed module 'chalk'

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,8 @@
+const chalk = require('chalk')
+
 module.exports = async (bot) => {
     if(bot.status && bot.status !== false) {
         bot.user.setActivity(bot.status, {type: bot.activity}) //Sets bot activity
     }
-    console.log("✅ " + bot.user.username + " is now working")
+    console.log("✅ " + bot.user.username + chalk.green(" is now working"))
 }
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const fs = require('fs')
+const c = require('chalk')
 
 //Discord client - I like "bot" more, then "client"
 const bot = new Discord.Client({
@@ -7,28 +8,31 @@ const bot = new Discord.Client({
     partials: ['MESSAGE', 'CHANNEL', 'REACTION']
 });
 
-const config = require('./config');
-const activites = ['PLAYING', 'WATCHING', 'COMPETING', 'LISTENING'] //Supported activites, discord.js supports more (but I don't care)
-bot.token = config.bot.token
+const config = require('./config'),
+activites = ['PLAYING', 'WATCHING', 'COMPETING', 'LISTENING'], //Supported activites, discord.js supports more (but I don't care)
+error = c.keyword('red').bold,
+kill = c.white('\nKilling process...'),
+warn = c.keyword('yellow').bold
+bot.token = config.bot.token;
 bot.prefix = config.bot.prefix;
 bot.status = config.bot.status;
-bot.activity = config.bot.activity.toUpperCase()
+bot.activity = config.bot.activity.toUpperCase();
 
 //Config check
 if(bot.token === '') { //Checks if you have entered bot token to config
-    console.log('⬜ ' + 'Bot token in config is empty!\nKilling process...');
+    console.log(error('Bot token in config is empty!') + kill);
     return process.exit(1);
 } else if (bot.prefix === '') { //Checks if you have entered bot prefix to config
-    console.log('⬜ ' + 'Bot prefix in config is empty!\nKilling process...');
+    console.log(error('Bot prefix in config is empty!') + kill);
     return process.exit(1);
 } else if (bot.status === '') { //Checks if you have enteredcustom status for bot to config
-    console.log('⚪ ' + 'Bot status in config was empty! Bot status was disabled.')
+    console.log(warn('Bot status in config was empty! Bot status was disabled.'));
     bot.status = false;
 } else if (bot.activity === '' && bot.status !== false) { //Checks if you have entered status activity type to config
-    console.log('⚪ ' + 'Bot activity type in config was empty! Activity type is now "playing"')
-    bot.activity = 'PLAYING'
+    console.log(warn('Bot activity type in config was empty! Activity type is now "playing"'));
+    bot.activity = 'PLAYING';
 } else if (!new Set(activites).has(bot.activity)) { //Checks if you have entered supported activity
-    console.log('⚪ ' + `"${bot.activity}" activity is not supported. Bot status was disabled.`)
+    console.log(warn(`"${bot.activity}" activity is not supported. Bot status was disabled.`));
     bot.status = false;
 }
 
